61db795ee2cc7a7a0a0ae3be3ce973048b600daa
Markus Bretterbauer <markus.bretterbauer@gmx.at>
2019-01-15 18:12:55 +0100

    Added buttons (not working yet) and request-entity

---
+++ src/app/entities/add-bill-request.ts
@@ -0,0 +1,3 @@
+export class AddBillRequest {
+  reservationIds: number[];
+}
--- src/app/pages/reservation-pages/reservation/reservation.component.ts
+++ src/app/pages/reservation-pages/reservation/reservation.component.ts
@@ -4,6 +4,8 @@ import {DomSanitizer} from '@angular/platform-browser';
 import {Reservation} from '../../../entities/reservation';
 import {ReservationService} from '../../../services/reservation.service';
 import {Router} from '@angular/router';
+import {Observable} from 'rxjs';
+import {BillService} from '../../../services/bill.service';
 
 @Component({
   selector: 'app-reservation',
@@ -15,10 +17,12 @@ export class ReservationComponent implements OnInit {
   @ViewChild(MatSort) sort: MatSort;
 
   allReservations = new MatTableDataSource([] as Reservation[]);
-  displayedColumns: String[] = ['Id', 'startDate', 'End Datum', 'Kunde', 'Zimmer'];
+  displayedColumns: String[] = ['Id', 'startDate', 'End Datum', 'Kunde', 'Zimmer', 'actions'];
+
+  private clickedAction = false;
 
   constructor(private reservationService: ReservationService, iconRegistry: MatIconRegistry, sanitizer: DomSanitizer,
-              private router: Router) {
+              private router: Router, private billService: BillService) {
     iconRegistry.addSvgIcon('add', sanitizer.bypassSecurityTrustResourceUrl('assets/baseline-add-24px.svg'));
   }
 
@@ -55,8 +59,28 @@ export class ReservationComponent implements OnInit {
   }
 
   openDetailForReservation(row: Reservation) {
-    console.log(row.id);
+    if (!this.clickedAction) {
+      console.log(row.id);
+      this.router.navigate(['reservations', 'detail', row.id], {skipLocationChange: false});
+    } else {
+      this.clickedAction = false;
+    }
+  }
 
-    this.router.navigate(['reservations', 'detail', row.id], {skipLocationChange: false});
+  createBill(row: Reservation) {
+    this.clickedAction = true;
+  }
+
+  deleteReservation(row: Reservation) {
+    /* const result: Observable<Reservation> = this.billService.a
+
+    // Create observer object
+    const myObserver = {
+      next: x => console.log('Observer got a next value: ' + x),
+      error: err => console.error('Observer got an error: ' + err),
+      complete: () => this.router.navigateByUrl('/reservations')
+    };
+
+    result.subscribe(myObserver); */
   }
 }
